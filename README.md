c604a73c31e5596307c9c782acfa6c7cf6b12c5f
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-23 12:42:10 -0800

    added README.md, tasks can be added to local storage, touched up appearance

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Work Day Scheduler Starter Code
-# daily-work-scheduler
+# Work Day Scheduler
+![markdown logo](screen_shot.png)
+
+## How it works 
+* This is a program where you can input tasks for a given hour.
+* tasks blocks are color coded to show you which is task is current(red), past(gray), and in the future(green). 
+* The current date is displayed on the top of the page. 
+* All items are saved in local storage if saved button is clicked. 
+* Erase the content and click the save button to erase a task. 
+## Link
+https://abarragan89.github.io/daily-work-scheduler/
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 "use strict";
 
 $(document).ready(function () {
-    // Check local storage
-    let savedTasks = JSON.parse(localStorage.getItem("personalTasks"));
-    console.log(savedTasks)
-    if (!savedTasks) {
-        savedTasks = []
-    }
-    // Get the hours in the p tags into an array
-    const paraEls = $(".row p");
-    // an arrray of the hours
-    let hours = paraEls.text();
-    hours = hours.split("m");
-    console.log(hours)
-
     // add current date in header
     const getDate = function () {
         const options = {
@@ -26,23 +13,22 @@ $(document).ready(function () {
         $("#currentDay").text(formatDate)
     }
     getDate();
-    setInterval(getDate, (1000 * 60) * 30)
+    setInterval(getDate, (1000 * 60) * 30);
 
     // determine textarea background color
     const addBgColor = function () {
         let today = new Date();
         let hour = today.getHours();
         console.log("hour " + hour)
-
+        // Get an array of each timeblock 
         const timeBlocks = $(".container-fluid").children();
         for (let i = 0; i < timeBlocks.length; i++) {
             let pEl = $(timeBlocks[i]).find("p").text();
             pEl = parseInt(pEl);
-            // add 12 hours if less than 6 to keep 24 hour. 
+            // add 12 hours if less than 6 to keep 24 hour or comparison with javascript date object. 
             if (pEl < 6) {
                 pEl += 12;
             }
-            console.log(pEl)
             // compare hour with current hour and change styles
             if (pEl < hour) {
                 $(timeBlocks[i]).find("textarea")
@@ -56,57 +42,32 @@ $(document).ready(function () {
             }
         }
     }
-    // addBgColor();
-
     // Save to local storage
-    // event.target is what triggered it, 'this' is what the eventListener is attached to. This is the second parameter in the 'on' method. 
-
-    const saveTasks = function (event) {
+    const saveTasks = function () {
         const task = $(this).parent("div").siblings("div").find("textarea").val();
         const taskHour = $(this).parent("div").siblings("div").find("p").text();
-        // debugger;
-        if (savedTasks.length == 0) {
-            savedTasks.push({ hour: taskHour, task: task });
-            // } else if () {
-
-            // see if taskHour is equal to one of 
-
-        } else if (savedTasks.length > 0) {
-            for (let i = 0; i < savedTasks.length; i++) {
-                if (savedTasks[i].hour == taskHour) {
-                    savedTasks[i].task = task;
-                    break;
-                } 
-            }
-            for (let i = 0; i < hours.length; i++) {
-                if (savedTasks[i].hour !== taskHour) {
-                    savedTasks.push({ hour: taskHour, task: task });
-                }
-            }
-
-            
-            console.log(savedTasks)
+        localStorage.setItem(taskHour, task)
             console.log(task)
             console.log(taskHour)
-            localStorage.setItem("personalTasks", JSON.stringify(savedTasks))
-        }
     }
     $(".row").on("click", "button", saveTasks);
-
     // Load tasks from local storage
-    // const load = function () {
-    //     for (let i = 0; i < savedTasks.length; i++) {
-    //         for (let j = 0; j < hours.length; i++) {
-    //             if (savedTasks[i].hour == hours[j]) {
-    //                 $(paraEls[j]).parent().siblings("div").find("textarea").val(savedTasks[i].task)
-    //             }
-    //         }
-    //     }
-
-    // }
-    // load();
-
-
-
-    ///////////////////////////     HELPER FUNCTIONS    /////////////////////////////
+    const load = function () {
+        // load the AM tasks
+        for (let i = 9; i < 13; i++)
+        if (localStorage.getItem(i + "am")) {
+            let task = localStorage.getItem(i + "am")
+            console.log(task)
+            $(`p:contains(${i + 'am'})`).parent("div").siblings("div").find("textarea").val(task)
+        }
+        // load the PM tasks
+        for (let i = 1; i < 6; i++)
+        if (localStorage.getItem(i + "pm")) {
+            let task = localStorage.getItem(i + "pm")
+            console.log(task)
+            $(`p:contains(${i + 'pm'})`).parent("div").siblings("div").find("textarea").val(task)
+        }
+}
+load();
+addBgColor();
 })
